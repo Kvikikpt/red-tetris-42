152a1453440bd9c27ab32b23999c53efb4d74b08
Kvikikpt <56595828+Kvikikpt@users.noreply.github.com>
2021-02-15 18:10:36 +0300

    socket-io done

--- src/client/containers/app.js
+++ src/client/containers/app.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { io } from "socket.io-client";
 
 
 const App = ({message}) => {
+  const [response, setResponse] = React.useState("");
+  const URL = "http://localhost:3000";
+
+  React.useEffect(() => {
+    const socket = io(URL, {
+      withCredentials: true,
+    });
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    });
+  }, []);
+
   return (
-    <span>{message}</span>
+    <p>
+      It's <time dateTime={response}>{response}</time>
+    </p>
   )
 }
 
--- src/server/main.js
+++ src/server/main.js
@@ -1,3 +1,39 @@
-import params  from '../../params'
-import * as server from './index'
-server.create(params.server).then( () => console.log('not yet ready to play tetris with U ...') )
+const express = require('express');
+const cors = require('cors')
+
+port = 3000;
+
+const app = express();
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
+app.use(cors())
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  let interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});

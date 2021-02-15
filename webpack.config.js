3d795b03381c8e84c7c952eaeb74357ff31e963a
Kvikikpt <56595828+Kvikikpt@users.noreply.github.com>
2021-02-15 16:36:08 +0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/client/index.js',
+
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["es2015", "react", "stage-0"]
+          }
+        }
+      }
+    ]
+  }
+};
